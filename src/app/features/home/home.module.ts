5772d17dbf1d0405d377a724ba306e2e951b0bf9
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2021-11-23 23:17:55 +0500

    add: add "components" folder to home module.

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { LeftBarParentComponent } from './left-bar-parent/left-bar-parent.component';
 import { RightBarComponent } from './right-bar/right-bar.component';
 import { MiddleBarParentComponent } from './middle-bar/middle-bar-parent/middle-bar-parent.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BestsellersComponent } from './middle-bar/bestsellers/bestsellers.component';
 import { HomeComponent } from './home.component';
+import { FreeContentBoxComponent } from './components/free-content-box/free-content-box.component';
 
 @NgModule({
   declarations: [
-    LeftBarParentComponent,
     RightBarComponent,
     MiddleBarParentComponent,
     BestsellersComponent,
     HomeComponent,
+    FreeContentBoxComponent,
   ],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })

887c4571f748ab706202d7d0b2a5d7d96be24c88
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2021-11-24 08:54:22 +0500

    fix: fix home component grid

--- src/app/features/home/components/bestsellers/bestsellers.component.ts
+++ src/app/features/home/components/bestsellers/bestsellers.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bestsellers',
   templateUrl: './bestsellers.component.html',
-  styleUrls: ['./bestsellers.component.scss']
+  styleUrls: ['./bestsellers.component.scss'],
 })
-export class BestsellersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class BestsellersComponent {
+  constructor() {}
 }

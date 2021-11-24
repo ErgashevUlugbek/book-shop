29d30c8c6a605fa2be54660553f592a56fe00705
Ergashev Ulug`bek <57306599+ErgashevUlugbek@users.noreply.github.com>
2021-11-24 08:06:34 +0500

    delete: middle-bar-parent component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { RightBarComponent } from './right-bar/right-bar.component';
-import { MiddleBarParentComponent } from './middle-bar/middle-bar-parent/middle-bar-parent.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { BestsellersComponent } from './middle-bar/bestsellers/bestsellers.component';
+import { BestsellersComponent } from './components/bestsellers/bestsellers.component';
 import { HomeComponent } from './home.component';
 import { FreeContentBoxComponent } from './components/free-content-box/free-content-box.component';
+import { RecentReleasesComponent } from './components/recent-releases/recent-releases.component';
 
 @NgModule({
   declarations: [
     RightBarComponent,
-    MiddleBarParentComponent,
     BestsellersComponent,
     HomeComponent,
     FreeContentBoxComponent,
+    RecentReleasesComponent,
   ],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
